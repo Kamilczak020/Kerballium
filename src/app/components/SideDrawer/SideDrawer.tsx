93890390342634e2acc4a7d2886b1286d6139dba
Kamilczak020 <kamilczak020@gmail.com>
2018-03-01 23:54:03 +0100

    Started developing drawer

--- src/app/components/SideDrawer/SideDrawer.tsx
+++ src/app/components/SideDrawer/SideDrawer.tsx
@@ -1,17 +1,23 @@
 import * as React from 'react';
 import * as style from './style.css';
 import * as classNames from 'classnames/bind';
+import { STORE_UI } from '../../constants/stores';
+import { UIStore } from '../../stores';
+import { inject, observer } from 'mobx-react';
 
 // Bind our classnames and styles
 let cx = classNames.bind(style);
 
-export interface SideDrawerProps {
-    open: boolean;
-}
-
-export class SideDrawer extends React.Component<SideDrawerProps> {
+@inject(STORE_UI)
+@observer
+export class SideDrawer extends React.Component {
     public render() {
-        const classes = cx({'sideDrawer': true, 'drawerExtended': this.props.open });
+        const uiStore = this.props[STORE_UI] as UIStore;
+
+        const classes = cx({
+            'drawerClosed': !uiStore.uiState.drawerOpen,
+            'sideDrawer': true,
+        });
 
         return (
             <div className={classes}>
