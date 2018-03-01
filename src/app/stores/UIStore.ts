fb623cde7a41f1b352747e7aa94961d3255f7836
Kamilczak020 <kamilczak020@gmail.com>
2018-03-01 23:56:00 +0100

    Created UI Store

--- src/app/stores/UIStore.ts
+++ src/app/stores/UIStore.ts
@@ -1,8 +1,14 @@
 import { action, computed, observable } from 'mobx';
+import { UIModel } from '../models/UIModel';
 
 export class UIStore {
     @observable
     public uiState: UIModel;
+
+    @action
+    public toggleDrawer(): void {
+        this.uiState.drawerOpen = !this.uiState.drawerOpen;
+    }
 
     constructor(fixture: UIModel) {
         this.uiState = fixture;
--- src/app/stores/index.ts
+++ src/app/stores/index.ts
@@ -1,1 +1,2 @@
 export { RouterStore } from './RouterStore';
+export { UIStore } from './UIStore';
