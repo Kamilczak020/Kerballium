413ae9a857571c1f9eb9d925039086ac1eb15a1b
Kamilczak020 <kamilczak020@gmail.com>
2018-03-02 16:40:03 +0100

    Added new stores to constants

--- src/app/constants/stores.ts
+++ src/app/constants/stores.ts
@@ -1,2 +1,5 @@
 export const STORE_ROUTER = 'router';
-export const STORE_UI = 'uistore';
+export const STORE_UI = 'ui';
+export const STORE_PLANNER_SATELLITE = 'satellitePlanner';
+export const STORE_INFORMATION_PLANET = 'planetInformation';
+export const STORE_PLANNER_TRIP = 'tripPlanner';
